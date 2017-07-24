1d3b3b4fbc1f35325a70361e0f6d532042e726d3
Akshay Naik <akshaynaik404@gmail.com>
2017-07-24 17:27:03 +0530

    Revert CRA project back to its original state and add yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home/Home';
-import OrderReview from './components/OrderReview/OrderReview';
-import NotFound from './components/NotFound/NotFound';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route path="/shop" component={Home}></Route>
-            <Route path="/review" component={OrderReview}></Route>
-            <Route path="*" component={NotFound}></Route>
-          </Switch>
-        </Router>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
